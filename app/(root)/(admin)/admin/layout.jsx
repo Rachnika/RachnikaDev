34771c17f652f2d61d9516c43f08994226660bd3
Lokesh Kumar <lokeshkmr30697@gmail.com>
2025-07-31 12:59:05 +0530

    cloudinary setup & media upload file done

--- app/(root)/(admin)/admin/layout.jsx
+++ app/(root)/(admin)/admin/layout.jsx
@@ -14,7 +14,7 @@ const layout = ({children}) => {
     <SidebarProvider>
         <AppSideBar/>
         <main className='md:w-[calc(100vw-16rem)]'>
-        <div className='pt-[70px] px-8 min-h-[calc(100vh-40px)] pb-10'>
+        <div className='pt-[90px] px-8 min-h-[calc(100vh-40px)] pb-10'>
             <Topbar/>
             {children}
         </div>
