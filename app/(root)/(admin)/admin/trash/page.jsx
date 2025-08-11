40f79ea5905633e35e7caf18b58857cd0502dcd6
Lokesh Kumar <lokeshkmr30697@gmail.com>
2025-08-11 10:43:20 +0530

    coupon section done

--- app/(root)/(admin)/admin/trash/page.jsx
+++ app/(root)/(admin)/admin/trash/page.jsx
@@ -8,7 +8,7 @@ import { ADMIN_CATEGORY_ADD, ADMIN_CATEGORY_EDIT, ADMIN_CATEGORY_SHOW, ADMIN_DAS
 import DatatableWrapper from "@/components/Application/Admin/DatatableWrapper";
 import { useCallback, useMemo } from "react";
 import { columnConfig } from "@/lib/helperFunction";
-import { DT_CATEGORY_COLUMN, DT_PRODUCT_COLUMN, DT_PRODUCT_VARIANT_COLUMN } from "@/lib/column";
+import { DT_CATEGORY_COLUMN, DT_COUPON_COLUMN, DT_PRODUCT_COLUMN, DT_PRODUCT_VARIANT_COLUMN } from "@/lib/column";
 import EditAction from "@/components/Application/Admin/EditAction";
 import DeleteAction from "@/components/Application/Admin/DeleteAction";
 import { useSearchParams } from "next/navigation";
@@ -42,6 +42,13 @@ const TRASH_CONFIG={
     fetchUrl:'/api/product-variant',
     exportUrl:'/api/product-variant/export',
     deleteUrl:'/api/product-variant/delete'
+  },
+  coupon:{
+    title:'Coupon Trash',
+    columns:DT_COUPON_COLUMN,
+    fetchUrl:'/api/coupon',
+    exportUrl:'/api/coupon/export',
+    deleteUrl:'/api/coupon/delete'
   },
 }
 
